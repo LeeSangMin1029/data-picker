0f001a03ed1207134830963dcf0ff33d255d3fe3
DESKTOP-3FIHK6R\skswo <kieun.kwon@moyusnvc.com>
2022-08-30 04:31:17 +0900

    최상단 컴포넌트에 theme 적용
    - body에도 테마가 적용되도록 함

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
-
-import "./index.css";
+import { WrapperThemeProvider } from "./themes";
 import App from "./App";
 
-render(() => <App />, document.getElementById("root"));
+render(
+  () => (
+    <WrapperThemeProvider>
+      <App />
+    </WrapperThemeProvider>
+  ),
+  document.getElementById("root")
+);
