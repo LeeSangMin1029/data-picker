98ab1a92a9e6e726cbea00497c8ed7de5b004e72
DESKTOP-3FIHK6R\skswo <kieun.kwon@moyusnvc.com>
2022-08-30 04:34:02 +0900

    Global 스타일 적용
    - 3개의 구역으로 나눠서 개발 진행
    - props.theme()는 createSignals로 만들어졌기 때문에 호출하는 방식으로만 값을 가져올 수 있다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+// 총 3개의 구역으로 나눠서 개발
+// 1. APP(header, container)
+// 2. header(theme, data-picker)
+// 3. container(date-realtime)
+import Header from "./Header";
+import Container from "./Container";
+import { createGlobalStyles } from "solid-styled-components";
+
+const GlobalStyles = () => {
+  const Styles = createGlobalStyles`
+  body{
+    background: ${(props) => props.theme().color};
+    transition: 200ms ease-in-out;
+  }
+  * {
+    margin: 0px;
+    padding: 0px;
+  }
+  ul, ol {
+    list-style: none;
+  }
+  a {
+    text-decoration: none;
+  }
+  `;
+  return <Styles />;
+};
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+      <Container />
+    </>
+  );
 }
 
 export default App;
