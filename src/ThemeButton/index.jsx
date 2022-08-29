641486e829da52d66fa2da9906925d2a259410a2
DESKTOP-3FIHK6R\skswo <kieun.kwon@moyusnvc.com>
2022-08-30 04:35:38 +0900

    슬라이더 버튼 제작
    - 클릭 시 좌, 우 이동하면서 기능 동작

---
+++ src/ThemeButton/index.jsx
@@ -0,0 +1,51 @@
+import { styled } from "solid-styled-components";
+import { useTheme } from "../themes";
+
+const Switch = styled.label`
+  position: relative;
+  display: flex;
+
+  width: 50px;
+  height: 26px;
+`;
+
+const NoneInput = styled.input`
+  display: none;
+  &:checked + span:before {
+    left: 26px;
+    background-color: #3368ff;
+  }
+`;
+
+const Slider = styled.span`
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: gray;
+  border-radius: 14px;
+  &:before {
+    content: "";
+    position: absolute;
+    width: 20px;
+    height: 20px;
+    background-color: white;
+    left: 4px;
+    top: 3px;
+    border-radius: 100%;
+    transition: 200ms ease-in-out;
+  }
+`;
+const ThemeButton = () => {
+  const [theme, { toggleTheme }] = useTheme();
+  return (
+    <Switch>
+      <NoneInput type="checkbox" onClick={toggleTheme} />
+      <Slider />
+    </Switch>
+  );
+};
+
+export default ThemeButton;
