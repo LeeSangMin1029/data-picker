58d3816ed92dd658b19b667eaafd49cab54d0e03
DESKTOP-3FIHK6R\skswo <kieun.kwon@moyusnvc.com>
2022-08-30 17:37:01 +0900

    modal 창 생성 및 토글 기능 적용
    - solid-styled-components가 solidjs의 Directives기능을 지원하지 않아서 ref기능을 사용
    - solidjs의 코드를 분석할 예정

--- src/DataPicker/index.jsx
+++ src/DataPicker/index.jsx
@@ -1,17 +1,34 @@
+import { createSignal, Show } from "solid-js";
+import { Portal } from "solid-js/web";
 import { styled } from "solid-styled-components";
 import { getCurrentDate } from "./date";
+import DateModal from "../DateModal";
 
 const StyledDiv = styled.div`
   align-self: center;
   border: 1px solid #ff9090;
   box-shadow: 2px 2px 5px #969eb1;
-  padding: 10px;
   user-select: none;
+  padding: 10px;
   cursor: pointer;
 `;
 
+const [view, setView] = createSignal(false);
+const onClickEvent = () => {
+  setView(true);
+};
+
 const DataPicker = () => {
-  return <StyledDiv>{getCurrentDate()}</StyledDiv>;
+  return (
+    <>
+      <StyledDiv onClick={[onClickEvent]}>{getCurrentDate()}</StyledDiv>
+      <Show when={view()} fallback={<></>}>
+        <Portal>
+          <DateModal setView={setView} />
+        </Portal>
+      </Show>
+    </>
+  );
 };
 
 export default DataPicker;
---
+++ src/DateModal/index.jsx
@@ -0,0 +1,49 @@
+import { createComponent } from "solid-js";
+import { styled } from "solid-styled-components";
+import clickOutside from "../utils";
+
+const Modal = styled.div`
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+`;
+
+const Contents = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  width: 600px;
+  height: 600px;
+  background-color: white;
+  border-radius: 10px;
+  transform: translateX(-50%) translateY(-50%);
+`;
+
+const DateModal = ({ setView }) => {
+  // 해당 코드는 어떻게 동작하는지 잘 모르겠다.
+  // use:something 이 기능을 대신하지만 내부적으로 컴파일을 했을 때 나오는 결과가 다르다.
+  // solid-styled-components라이브러리에서 해당 기능을
+  // 지원하지 않는다. 그래서 대체제로 아래 코드를 사용한다.
+  // https://github.com/solidjs/solid-styled-components/issues/4
+  createComponent(Contents, {
+    ref(el) {
+      clickOutside(el, () => setView(false));
+    },
+  });
+  return (
+    <>
+      <Modal>
+        <Contents />
+      </Modal>
+    </>
+  );
+};
+
+export default DateModal;
