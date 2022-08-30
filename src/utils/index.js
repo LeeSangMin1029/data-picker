2d567c3fbcecfea56a363d61a6adddc3d409de11
DESKTOP-3FIHK6R\skswo <kieun.kwon@moyusnvc.com>
2022-08-31 00:35:25 +0900

    한국 날짜 출력되도록 수정
    - 미리 9시간을 빼서 시간을 맞췄다.

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,6 +10,8 @@ const clickOutside = (el, accessor) => {
 
 const getCurrentDate = () => {
   const currentDate = new Date();
+  // toISOString함수를 호출하면 9시간이 마이너스 된 상태로 출력이 되기 때문에 미리 더해준다.
+  currentDate.setHours(currentDate.getHours() + 9);
   return currentDate.toISOString().split("T")[0].split("-");
 };
 const addZ = (n) => (n < 10 ? "0" + n : "" + n);
